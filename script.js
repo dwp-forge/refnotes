16dcdc7e9d3900da33bb5337adf5919ce34a4e1d
Mykola Ostrovskyy <spambox03@mail.ru>
2009-03-14 12:59:47 +0000

    r104
    * Preview methods are combined into an object
    ! Maximum preview width in IE

--- script.js
+++ script.js
@@ -1,103 +1,153 @@
 (function() {
     var cssComp = document.compatMode && (document.compatMode == "CSS1Compat");
     var canvas = document.getElementsByTagName(cssComp ? "html" : "body")[0];
-    var preview = null;
+    var floater = null;
+    var floaterWidth = 0;
+    var floaterHeight = 0;
     var tracking = false;
     var shown = false;
     var timer = null;
 
-    function createPreview() {
-        preview = document.createElement('div');
-        preview.id = 'insitu__fn';
-        preview.className = 'insitu-footnote JSpopup dokuwiki';
-        preview.style.position = 'absolute';
-        preview.style.left = '0px';
-        preview.style.top = '-100px';
+    var preview = {
+        createFloater: function() {
+            floater = document.createElement('div');
+            floater.id = 'insitu__fn';
+            floater.className = 'insitu-footnote JSpopup dokuwiki';
+            floater.style.position = 'absolute';
+            floater.style.left = '0px';
+            floater.style.top = '-100px';
 
-        // autoclose on mouseout - ignoring bubbled up events
-        addEvent(preview, 'mouseout', function(e) {
-            if (e.target != preview) {
-                e.stopPropagation();
-                return;
-            }
-            // check if the element was really left
-            var offsetX = e.pageX ? e.pageX - findPosX(preview) : e.offsetX;
-            var offsetY = e.pageY ? e.pageY - findPosY(preview) : e.offsetY;
-            var msieDelta = e.pageX ? 0 : 1;
-            var width = preview.offsetWidth - msieDelta;
-            var height = preview.offsetHeight - msieDelta;
-            if ((offsetX > 0) && (offsetX < width) && (offsetY > 0) && (offsetY < height)) {
-                // we're still inside boundaries
-                e.stopPropagation();
-                return;
-            }
-            hidePreview();
-        });
-        document.body.appendChild(preview);
-    }
+            // autoclose on mouseout - ignoring bubbled up events
+            addEvent(floater, 'mouseout', function(e) {
+                if (e.target != floater) {
+                    e.stopPropagation();
+                    return;
+                }
+                // check if the element was really left
+                var offsetX = e.pageX ? e.pageX - findPosX(floater) : e.offsetX;
+                var offsetY = e.pageY ? e.pageY - findPosY(floater) : e.offsetY;
+                var msieDelta = e.pageX ? 0 : 1;
+                var width = floater.offsetWidth - msieDelta;
+                var height = floater.offsetHeight - msieDelta;
+                if ((offsetX > 0) && (offsetX < width) && (offsetY > 0) && (offsetY < height)) {
+                    // we're still inside boundaries
+                    e.stopPropagation();
+                    return;
+                }
+                preview.hide();
+            });
+            document.body.appendChild(floater);
+        },
 
-    function getPreview() {
-        if (!preview) {
-            preview = $('insitu__fn');
-            if (!preview) {
-                createPreview();
+        getFloater: function() {
+            if (!floater) {
+                floater = $('insitu__fn');
+                if (!floater) {
+                    this.createFloater();
+                }
             }
-        }
-        return preview;
-    }
+            return floater;
+        },
 
-    function showPreview() {
-        var preview = getPreview();
-        if (preview) {
-            preview.style.visibility = 'visible';
-            preview.style.display = '';
-        }
-        shown = true;
-    }
-
-    function hidePreview() {
-        if (shown) {
-            var preview = getPreview();
-            if (preview) {
-                preview.style.display = 'none';
+        measureFloater: function() {
+            floaterWidth = 0;
+            floaterHeight = 0;
+            var floater = this.getFloater();
+            if (floater) {
+                var width = window.event ? floater.clientWidth : floater.offsetWidth;
+                var height = window.event ? floater.clientHeight : floater.offsetHeight;
+                if (width && height) {
+                    // add CSS padding
+                    floaterWidth = 10 + width;
+                    floaterHeight = 10 + height;
+                }
             }
+        },
+
+        setNoteId: function(id) {
+            var floater = this.getFloater();
+            // locate the note span element
+            var note = $(id + ':text');
+            if (!floater || !note) {
+                return false;
+            }
+            // get the note HTML
+            var html = new String(note.innerHTML);
+            // prefix ids on any elements to ensure they remain unique
+            html.replace(/\bid=\"(.*?)\"/gi, 'id="refnotes-preview-$1');
+
+            // now put the content into the floater
+            floater.innerHTML = html;
+
+            // display hidden tooltip so we can measure it's size
+            floater.style.visibility = 'hidden';
+            floater.style.display = '';
+            floater.style.left = '0px';
+            floater.style.top = '0px';
+
+            this.measureFloater();
+            if (floaterWidth && ((floaterWidth / canvas.clientWidth) > 0.45)) {
+                // simulate max-width in IE
+                floater.style.width = '40%';
+                this.measureFloater();
+            }
+
+            return true;
+        },
+
+        show: function() {
+            var floater = this.getFloater();
+            if (floater) {
+                floater.style.visibility = 'visible';
+                floater.style.display = '';
+            }
+            shown = true;
+        },
+
+        hide: function() {
+            var floater = this.getFloater();
+            if (floater) {
+                floater.style.display = 'none';
+                floater.style.width = '';
+            }
+            floaterWidth = 0;
+            floaterHeight = 0;
             shown = false;
+        },
+
+        move: function(x, y, dx, dy) {
+            var floater = this.getFloater();
+            if (!floater) {
+                return;
+            }
+            var windowWidth = canvas.clientWidth + canvas.scrollLeft;
+            var windowHeight = canvas.clientHeight + canvas.scrollTop;
+            if (!floaterWidth || !floaterHeight) {
+                this.measureFloater();
+            }
+            x += dx;
+            if ((x + floaterWidth) > windowWidth) {
+                x -= dx + 2 + floaterWidth;
+            }
+            y += dy;
+            if ((y + floaterHeight) > windowHeight ) {
+                y -= dy + 2 + floaterHeight;
+            }
+            floater.style.left = x + 'px';
+            floater.style.top = y + 'px';
         }
+    };
+
+    function getNoteId(e) {
+        return e.target.href.replace(/^.*?#([\w:]+)$/gi, '$1');
     }
 
-    function movePreview(x, y, dx, dy) {
-        var preview = getPreview();
-        if (!preview) {
-            return;
-        }
-        var windowWidth = canvas.clientWidth + canvas.scrollLeft;
-        var windowHeight = canvas.clientHeight + canvas.scrollTop;
-        var previewWidth = 10 + window.event ? preview.clientWidth : preview.offsetWidth;
-        var previewHeight = 10 + window.event ? preview.clientHeight : preview.offsetHeight;
-
-        x += dx;
-        if ((x + previewWidth) > windowWidth) {
-            x -= dx + 2 + previewWidth;
-        }
-        y += dy;
-        if ((y + previewHeight) > windowHeight ) {
-            y -= dy + 2 + previewHeight;
-        }
-        preview.style.left = x + 'px';
-        preview.style.top = y + 'px';
+    function getEventX(e) {
+        return e.pageX ? e.pageX : e.offsetX;
     }
 
-    function getNote(e) {
-        var id = e.target.href.replace(/^.*?#([\w:]+)$/gi, '$1');
-        // locate the note span element
-        var note = $(id + ':text');
-        if (!note) {
-            return;
-        }
-        // get the note HTML
-        var html = new String(note.innerHTML);
-        // prefix ids on any elements to ensure they remain unique
-        return html.replace(/\bid=\"(.*?)\"/gi, 'id="refnotes-preview-$1');
+    function getEventY(e) {
+        return e.pageY ? e.pageY : e.offsetY;
     }
 
     function log(message) {
@@ -111,49 +161,23 @@
         popup: {
             show: function(e) {
                 plugin_refnotes.tooltip.hide(e);
-
-                var preview = getPreview();
-                var note = getNote(e);
-                if (!preview || !note) {
+                if (!preview.setNoteId(getNoteId(e))) {
                     return;
                 }
-                // now put the content into the wrapper
-                preview.innerHTML = note;
-
-                // display hidden tooltip so we can measure it's size
-                preview.style.visibility = 'hidden';
-                preview.style.display = '';
-                preview.style.left = '0px';
-                preview.style.top = '0px';
-
-                // position the div and make it visible
-                var x = e.pageX ? e.pageX : e.offsetX;
-                var y = e.pageY ? e.pageY : e.offsetY;
-                movePreview(x, y, 2, 2);
-                showPreview();
+                // position the floater and make it visible
+                preview.move(getEventX(e), getEventY(e), 2, 2);
+                preview.show();
             }
         },
 
         tooltip: {
             show: function(e) {
                 plugin_refnotes.tooltip.hide(e);
-
-                var preview = getPreview();
-                var note = getNote(e);
-                if (!preview || !note) {
+                if (!preview.setNoteId(getNoteId(e))) {
                     return;
                 }
-                // now put the content into the wrapper
-                preview.innerHTML = note;
-
-                // display hidden tooltip so we can measure it's size
-                preview.style.visibility = 'hidden';
-                preview.style.display = '';
-                preview.style.left = '0px';
-                preview.style.top = '0px';
-
                 // start tooltip timeout
-                timer = setTimeout(function(){ showPreview(); }, 500);
+                timer = setTimeout(function(){ preview.show(); }, 500);
                 tracking = true;
             },
 
@@ -162,17 +186,12 @@
                     clearTimeout(timer);
                     tracking = false;
                 }
-                hidePreview();
+                preview.hide();
             },
 
             track: function(e) {
                 if (tracking) {
-                    var x = e.pageX ? e.pageX : e.offsetX;
-                    var y = e.pageY ? e.pageY : e.offsetY;
-                    //var x = window.event ? event.clientX + canvas.scrollLeft : e.pageX;
-                    //var y = window.event ? event.clientY + canvas.scrollTop : e.pageY;
-
-                    movePreview(x, y, 10, 12);
+                    preview.move(getEventX(e), getEventY(e), 10, 12);
                 }
             }
         }
