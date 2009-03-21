2f6608b6bc9c08265e71bf1ec5ef045ebb0704b5
Mykola Ostrovskyy <spambox03@mail.ru>
2009-03-21 13:34:10 +0000

    r115: Detection of info plugin is replaced by detection of the plugin manager

--- info.php
+++ info.php
@@ -11,17 +11,13 @@ function refnotes_getinfo($component = '') {
     $info = array(
         'author' => 'Mykola Ostrovskyy',
         'email'  => 'spambox03@mail.ru',
-        'date'   => '2009-03-15',
+        'date'   => '2009-03-21',
         'name'   => 'RefNotes Plugin',
         'desc'   => 'Extended syntax for footnotes and references.',
         'url'    => 'http://code.google.com/p/dwp-forge/',
     );
     if ($component != '') {
-        $trace = debug_backtrace(false);
-        $infoPlugin = (array_key_exists('class', $trace[2]) &&
-                       ($trace[2]['class'] == 'syntax_plugin_info') &&
-                       ($trace[2]['function'] == '_plugins_xhtml'));
-        if (!$infoPlugin) {
+        if (($_REQUEST['do'] == 'admin') && !empty($_REQUEST['page']) && ($_REQUEST['page'] == 'plugin')) {
             $info['name'] .= ' (' . $component . ')';
         }
     }
